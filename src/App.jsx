17bb6b4f346f5ecbf4630da27a4a52aba9b61d67
Lauriene <laurienea840@gmail.com>
2025-04-22 08:59:07 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import react from './assets/react (1).png'
+export default function (){
+  return <div>
+    <img src={react} alt="" />
+   <h1>React</h1>
+   <p> A biblioteca para interfaces de usuário web e nativas.</p>
+   <button> Aprenda React</button> <button> Referência da API</button> <hr />
+   <h1> Crie interfaces de usuários de componentes</h1>
+   <p> React permite que você construa interfaces de usuário a partir de pedaços individuias chamados componenetes</p> <hr />
+
+   <h1>Escreva componentes com códigos de marcação</h1>
+   <p> Componentes React são funçoes JavaScript. A sintaxe de marcação é chamada de JSX. È uma extensão da sintaxe JavaScript popularizada pelo react. </p> <hr />
+   <h1> proximos passos</h1>
+   <ul>
+    <li>Uso de dados dinamicos no JSX</li>
+    <li>Criação de novos componentes</li>
+    <li>Estilização de novos componenetes</li>
+    <li>Uso de props e children</li>
+    <li>Uso de eventos em JavaScript</li>
+   </ul>
+  </div>
+}
